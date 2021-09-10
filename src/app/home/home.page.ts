8c79bd360a274f13a507a2d8e4f53f10fe6bc8c0
ionic-tester <JKABOO4@yahoo.com>
2021-09-10 09:01:43 -0500

    getCurrentPosition added to test geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  latitude: number;
+  longitude: number;
   constructor() {}
+
+  printCurrentPosition()
+    {
+      //console.log(this.locationService.checkGPSPermission());
+      Geolocation.getCurrentPosition().then((resp) => {
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+      }).catch((error) => {
+        alert('Error getting location. Please check Location Services and try again.');
+        console.log(error);
+      });
+    }
 
 }
